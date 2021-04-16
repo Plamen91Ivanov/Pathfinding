ca4525bde6c302636436d2342372af55788503bd
plamen <plamen91ivanov@abv.bg>
2021-04-16 16:35:44 +0300

    add

--- pathfinding/src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ pathfinding/src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Node from './Node/Node';
-import {djikstra,getNodesInShortestPathOrder} from '../algorithms/dijkstra'; 
+import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
 
 import './PathfindingVisualizer.css';
 
@@ -9,20 +9,20 @@ const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 25;
 
-export default class PathfindingVisualizer extends Component { 
-    constructor(){
-        super();
-        this.state = {
-            grid: [],
-            mouseIsPressed: false,
-        }
-    }
-    
-    componentDidMount(){
-        const grid = getInitialGrid();
-        this.setState({grid});
-    }
-    
+export default class PathfindingVisualizer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
+
+  componentDidMount() {
+    const grid = getInitialGrid();
+    this.setState({grid});
+  }
+
   handleMouseDown(row, col) {
     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
     this.setState({grid: newGrid, mouseIsPressed: true});
@@ -38,28 +38,39 @@ export default class PathfindingVisualizer extends Component {
     this.setState({mouseIsPressed: false});
   }
 
-  animateDjikstra(visitedNodesInOrder,nodesInShortestPathOrder) {
-      for (let i = 0; i < visitedNodesInOrder.length; i++) {
-          if(i === visitedNodesInOrder.length){
-              setTimeout(() => {
-                  this.animateShortestPath(nodesInShortestPathOrder);
-              }, 10 * i);
-              return;
-          }
-          setTimeout(() => {
-              const node = visitedNodesInOrder[i];
-              document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-visited';
-          }, 10 * i);
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
       }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-visited';
+      }, 10 * i);
+    }
   }
 
-  animateShortestPath(nodesInShortestPathOrder){
-      for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
-        setTimeout(() => {
-            const node = nodesInShortestPathOrder[i];
-            document.getElementById(`node-${node.row}-${node.col}`).className = 'node node-shortest-path';
-        }, 50 * i);          
-      }
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-shortest-path';
+      }, 50 * i);
+    }
+  }
+
+  visualizeDijkstra() {
+    const {grid} = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
   render() {
