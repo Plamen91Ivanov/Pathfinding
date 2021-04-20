9c1a881468909aa62953ebf4ec001760fff86e88
plamen <plamen91ivanov@abv.bg>
2021-04-20 13:24:15 +0300

    two algorithm finished

--- pathfinding/src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ pathfinding/src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -7,8 +7,8 @@ import './PathfindingVisualizer.css';
 
 const START_NODE_ROW = 5;
 const START_NODE_COL = 5;
-const FINISH_NODE_ROW = 5;
-const FINISH_NODE_COL = 10;
+const FINISH_NODE_ROW = 1;
+const FINISH_NODE_COL = 12;
 
 export default class PathfindingVisualizer extends Component {
   constructor() {
@@ -54,14 +54,14 @@ export default class PathfindingVisualizer extends Component {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
           this.animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
+        }, 100 * i);
         return;
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           'node node-visited';
-      }, 10 * i);
+      }, 100 * i);
     }
   }
 
--- pathfinding/src/algorithms/dfs.js
+++ pathfinding/src/algorithms/dfs.js
@@ -1,13 +1,14 @@
 export function dfs(grid, startNode, finishNode){
     const visitedNodes = [];
     const unvisiteNodes = getAllNodes(grid);
+    var unvisitedNeighbors = [];
     while(!!unvisiteNodes.length){
         const nextNode = startNode
         startNode.isVisited = true;
         visitedNodes.push(nextNode);
-        const adjNodes = getNextNode(nextNode, grid);
-        if (!!adjNodes.length) {
-            if (nextNode === finishNode) return visitedNodes;
+        //const adjNodes = getNextNode(nextNode, grid);
+        const adjNodes = getUnvisitedNeighbors(nextNode, grid,unvisitedNeighbors);
+        if (nextNode === finishNode) return visitedNodes;
         startNode = adjNodes.pop();
         if (startNode.isWall){
             startNode = adjNodes.pop();
@@ -18,10 +19,20 @@ export function dfs(grid, startNode, finishNode){
                         }
                 }
         };
-        } 
         unvisiteNodes.shift();
     }    
 }
+
+function getUnvisitedNeighbors(node, grid,unvisitedNeighbors){
+    const {col, row} = node;  
+    if (col > 0) unvisitedNeighbors.push(grid[row][col - 1]);
+    if (row < grid.length - 1) unvisitedNeighbors.push(grid[row + 1][col]);
+    if (col < grid[0].length - 1) unvisitedNeighbors.push(grid[row][col + 1]);
+    if (row > 0) unvisitedNeighbors.push(grid[row - 1][col]);
+  
+    return unvisitedNeighbors.filter(neighbor => !neighbor.isVisited);
+}
+
 
 function getNextNode(node, grid) {
     let nextNodes = [];
